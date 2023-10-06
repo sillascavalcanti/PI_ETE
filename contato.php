7f05f8f049a89429fcd16f91ef26cf9d3beb05ff
Sillas Cavalcanti <sillascavalcanti1@gmail.com>
2023-10-06 00:57:26 -0300

    Criacao dos requests para o banco de dados

--- contato.php
+++ contato.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/style/style.css">
+    <link rel="stylesheet" href="style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet">
@@ -18,10 +18,9 @@
             <img src="img/Logo.png" alt="Logo">
         </section>
         <section class="area_menu">
-            <a class="menu_nav" href="index.html">Inicio</a>
-            <a class="menu_nav" href="sobreNos.html">Sobre nós</a>
-            <a class="menu_nav" href="contato.html">Contato</a>
-            <!-- <a class="menu_nav" href="sugestao.html">Sugestão</a> -->
+            <a class="menu_nav" href="index.php">Inicio</a>
+            <a class="menu_nav" href="sobreNos.php">Sobre nós</a>
+            <a class="menu_nav" href="contato.php">Contato</a>
         </section>
     </nav>
     
@@ -33,9 +32,9 @@
             <p><span>E-mail</span>:contato@devlearning.com.br</p>
             <h3 class="sugestao_titulo">Sugestão</h3>
             <p class="sugestao">Adoraríamos ouvir suas sugestões e receber seu feedback para melhorar o Dev.Learning. Envie suas ideias:</p>
-            <form action="" class="formulario" onsubmit="salvar_sugestao()">
+            <form method="post" action="script/dev_learning.php" class="formulario">
                 <textarea name="sugestao" id="sugestao" cols="30" rows="10" class="texto_formulario" required></textarea>
-                <input type="email" placeholder="Exemplo@gmail.com" id="email_usuario" class="componente_formulario" required>
+                <input type="email" placeholder="Exemplo@gmail.com" name="email_usuario" class="componente_formulario" required>
                 <input type="submit" value="Enviar" class="botao_formulario">
             </form>
             
